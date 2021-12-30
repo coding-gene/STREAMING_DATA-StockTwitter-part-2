5673890d0867c43457dd0771db4ae791db158273
coding-gene <andricivan86@gmail.com>
2021-12-30 16:05:52 +0100

    Initial commit

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 import sqlite3
 import pandas as pd
+import logging
+import time
 
-conn = sqlite3.connect(r'C:\Users\Ivana\PycharmProjects\TwitterSentimentAnalysis\stock.db')
-df = pd.read_sql_query("SELECT * FROM gme_stock_data", conn)
-print(df)
+try:
+    connection = sqlite3.connect(r'C:\Users\Ivana\PycharmProjects\TwitterSentimentAnalysis\stock.db')
+    start_time = time.time()
+    logging.basicConfig(filename='logs.txt',
+                        filemode='a',
+                        format='%(asctime)s %(levelname)-8s %(message)s',
+                        level=logging.INFO,
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    logging.info(f'Pocetak izvrsavanja zadatka.')
+
+    df = pd.read_sql_query("SELECT * FROM gme_stock_data", connection)
+    print(df)
+
+except Exception:
+    logging.exception(f'Dogodila se greska sljedeceg sadrzaja:')
+    #stock.rollback_connection()
+else:
+    logging.info('Uspjesno izvrsen zadatak.')
+finally:
+    #stock.closing_connection()
+    logging.info(f'Obrada trajala: {time.strftime("%H sati, %M minuta i %S sekundi.", time.gmtime(time.time() - start_time))}\n')
