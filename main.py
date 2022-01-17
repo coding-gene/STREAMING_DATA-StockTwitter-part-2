aa72789375e666efcc33e2f71d92a398267cc563
Ivan Andrić <andricivan86@gmail.com>
2022-01-17 19:42:17 +0100

    Initial commit

--- main.py
+++ main.py
@@ -13,6 +13,13 @@ import dash_core_components as dcc
 import numpy as np
 from dash.dependencies import Input, Output
 
+#  twitt_cursor.execute('DELETE FROM tweet_sentiment_analysis;')
+#  twitt_cursor.execute('DROP TABLE IF EXISTS tweet_sentiment_analysis;')
+#  twitt_connection.commit()
+#  stock_cursor.execute('DELETE FROM gme_stock_data;')
+#  stock_cursor.execute('DROP TABLE IF EXISTS gme_stock_data;')
+#  stock_connection.commit()
+
 pd.set_option('display.max_rows', 40)
 pd.set_option('display.max_columns', 40)
 pd.set_option('display.width', 200)
@@ -25,29 +32,30 @@ logging.basicConfig(filename='logs.txt',
                     level=logging.INFO,
                     datefmt='%Y-%m-%d %H:%M:%S')
 logging.info(f'Job started.')
+
+twitt_connection = sqlite3.connect(r'C:\Users\Ivan\PycharmProjects\SCRAPING-StockTwitter\twitter.db')
+twitt_cursor = twitt_connection.cursor()
+stock_connection = sqlite3.connect(r'C:\Users\Ivan\PycharmProjects\SCRAPING-StockTwitter\stock.db')
+stock_cursor = stock_connection.cursor()
+
 # noinspection PyBroadException
 try:
-    twitt_connection = sqlite3.connect(r'C:\Users\Ivan\PycharmProjects\SCRAPING-StockTwitter\twitter.db')
-    twitt_cursor = twitt_connection.cursor()
-    #  twitt_cursor.execute('DELETE FROM tweet_sentiment_analysis;')
-    #  twitt_cursor.execute('DROP TABLE IF EXISTS tweet_sentiment_analysis;')
-    #  twitt_connection.commit()
+    app = dash.Dash(__name__)
+    # ----------------------------------------------------------------------------------------------------------------
+    # Import and merge stock & twitter data
     twitt_df = pd.read_sql_query('SELECT * FROM tweet_sentiment_analysis', twitt_connection)
-    twitt_df.to_excel(r'twitter_data.xlsx', index=False)
-
-    stock_connection = sqlite3.connect(r'C:\Users\Ivan\PycharmProjects\SCRAPING-StockTwitter\stock.db')
-    stock_cursor = stock_connection.cursor()
-    #  stock_cursor.execute('DELETE FROM gme_stock_data;')
-    #  stock_cursor.execute('DROP TABLE IF EXISTS gme_stock_data;')
-    #  stock_connection.commit()
+    print(twitt_df)
+    #  twitt_df.to_excel(r'twitter_data.xlsx', index=False)
     stock_df = pd.read_sql_query('SELECT * FROM gme_stock_data', stock_connection)
-    stock_df.to_excel(r'stock_data.xlsx', index=False)
+    print(stock_df)
+    #  stock_df.to_excel(r'stock_data.xlsx', index=False)
+    # ----------------------------------------------------------------------------------------------------------------
 except Exception:
     logging.exception(f'An error occurred during job performing:')
-    # stock.rollback_connection()
 else:
     logging.info('Job ended.')
 finally:
-    # stock.closing_connection()
+    twitt_connection.close()
+    stock_connection.close()
     logging.info(
         f'Job duration: {time.strftime("%H hours, %M minutes, %S seconds.", time.gmtime(time.time() - start_time))}\n')
