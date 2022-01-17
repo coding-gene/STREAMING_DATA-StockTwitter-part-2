1492f045b1a24ff8e942102ba5af1668e82e6e92
Ivan Andrić <andricivan86@gmail.com>
2022-01-17 19:27:21 +0100

    Initial commit

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 # https://github.com/anubhavanand12qw/STOCK-PRICE-PREDICTION-USING-TWITTER-SENTIMENT-ANALYSIS/blob/master/STOCK%20PREDICTION%20USING%20TWITTER%20SENTIMENT%20ANALYSIS%20PROJECT%20(FINAL)%20-%20Updated.ipynb
 # https://github.com/talaikis/StockTalk3
+# https://stackoverflow.com/questions/63589249/plotly-dash-display-real-time-data-in-smooth-animation
+# https://dash.plotly.com/live-updates
+# pipenv lock -r > requirements.txt
 import sqlite3
 import pandas as pd
 import logging
 import time
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+import numpy as np
+from dash.dependencies import Input, Output
 
 pd.set_option('display.max_rows', 40)
 pd.set_option('display.max_columns', 40)
@@ -26,7 +34,6 @@ try:
     #  twitt_connection.commit()
     twitt_df = pd.read_sql_query('SELECT * FROM tweet_sentiment_analysis', twitt_connection)
     twitt_df.to_excel(r'twitter_data.xlsx', index=False)
-    print(twitt_df)
 
     stock_connection = sqlite3.connect(r'C:\Users\Ivan\PycharmProjects\SCRAPING-StockTwitter\stock.db')
     stock_cursor = stock_connection.cursor()
@@ -35,10 +42,6 @@ try:
     #  stock_connection.commit()
     stock_df = pd.read_sql_query('SELECT * FROM gme_stock_data', stock_connection)
     stock_df.to_excel(r'stock_data.xlsx', index=False)
-    print(stock_df)
-
-
-
 except Exception:
     logging.exception(f'An error occurred during job performing:')
     # stock.rollback_connection()
